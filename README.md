38dfa782a24d73e671a73077f29d7d33b63d6aba
Serveto Anne-Marie <servetonguyen@outlook.com>
2022-10-22 11:51:36 +0200

    Added more in the css department when a task is done, changed the tag for the card and added a bit to the Readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ToDo-List
+An app that makes keeping track of your things to do easier. Basic but gets the job done !
--- src/components/card.js
+++ src/components/card.js
@@ -1,10 +1,10 @@
 const Card = ({ data, todoData, done, handleDelete, handleDone, id }) => {
     return (
-        <div className="Card">
+        <article className="Card">
             {done === true ? <p className="lineThrough">{todoData}</p> : <p>{todoData}</p>}
             {done === "false" ?<button className="doneBtn" onClick={() => handleDone(id)}>Done</button> : <noscript></noscript>}
             <button className="delBtn" onClick={() => handleDelete(id)}>Delete</button>
-        </div>
+        </article>
     );
 }
 
